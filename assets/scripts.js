8980771eb8d9293e2f7db519dc47cead864b7a1b
AlbertoST876 <albertost876.ast@gmail.com>
2022-06-27 11:59:50 +0200

    Eliminado los comentarios de CSS y JS

--- assets/scripts.js
+++ assets/scripts.js
@@ -1,11 +1,3 @@
-/**
- * @ Author: Alberto Sanchez Torreblanca
- * @ Create Time: 30-03-2022 09:11:38
- * @ Modified by: Alberto Sanchez Torreblanca
- * @ Modified time: 01-04-2022 20:03:02
- * @ Description: Script que esconde o muestra el menu cuando pulsamos en "Menu" en pantallas menores a 1000px
- */
-
 window.onload = function() {
     var menuBtn = document.querySelector('header nav span'),
         menu = document.querySelector('header nav ul');
