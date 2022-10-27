4fc9fb5db15c5709fd39176f00a38f3c2360e204
AlbertoST876 <albertost876.ast@gmail.com>
2022-10-27 21:59:34 +0200

    Release 2.1.0

--- src/functions/admin/tools/restorePassword.php
+++ src/functions/admin/tools/restorePassword.php
@@ -9,11 +9,8 @@ use Games\Classes\DB;
  */
 function restorePassword(): void {
     if ($_POST["newPassword1"] == $_POST["newPassword2"]) {
-        $connect = new DB();
-
-        $hash = password_hash($connect -> clearString($_POST["newPassword1"]), PASSWORD_DEFAULT);
-        
-        $connect -> Update("UPDATE users SET password = '$hash', restorePassword = 'F', lastPasswordChange = '" . date("Y/m/d H:i:s") . "' WHERE id = '" . $_SESSION["user"] -> getId() . "'");
+        $connect = new DB(); 
+        $connect -> Update("UPDATE users SET password = '" . password_hash($connect -> clearString($_POST["newPassword1"]), PASSWORD_DEFAULT) . "', restorePassword = 'F', lastPasswordChange = '" . date("Y/m/d H:i:s") . "' WHERE id = '" . $_SESSION["user"] -> getId() . "'");
     } else {
         echo "<p>Las contraseñas introducidas son diferentes, inténtalo de nuevo</p>";
     }
